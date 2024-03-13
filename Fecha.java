74554ce391a25c629b27e205024c073653b48727
JoelVelasco <daw14.2024@gmail.com>
2024-03-13 17:18:54 +0100

    Proyecto Documentado

--- Fecha.java
+++ Fecha.java
@@ -1,10 +1,32 @@
 package examen;
 
 public class Fecha {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * @author JoelVelasco
+	 * @verion version1.0
+	 */
+	public static boolean fechaValida;
+	
+/**
+ * 
+ * @param anio "es un parametro para los años"
+ * @param mes "es un parametro para los meses del año"
+ * @param dia "es un parametro para los dias"
+ * @return "devuelve un booleano"
+ */
+	public static boolean validarFecha(int anio, int mes, int dia) {
+		fechaValida = false;
+		if ((dia > 0) && (mes > 0) && (anio > 0)) {
+			if ((mes <= 12) && (dia <= 31)) {
+				if (mes == 2) {
+					fechaValida = dia <= 28;
+				} else if (mes % 2 == 0) {
+					fechaValida = dia <= 30;
+				} else {
+					fechaValida = dia <= 31;
+				}
+			}
+		}
+		return fechaValida;
 	}
-
 }
